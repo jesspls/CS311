2119c45116781d879ed5553f358b586e54d4a6e2
Jessica Haynes <jkhaynes@iastate.edu>
2015-07-07 16:58:58 -0500

    Larvae walk program completed

--- LarvaeWalk/src/larvaeWalk.java
+++ LarvaeWalk/src/larvaeWalk.java
@@ -4,29 +4,62 @@ import java.util.Scanner;
 
 public class larvaeWalk {
 
+	// represents all steps that need to be taken to visit each of cells
+	// neighbors
+	static int[][] move = { { -1, -1 }, { 0, -1 }, { 1, 0 }, { 1, 1 },
+			{ 0, 1 }, { -1, 0 } };
+
+	// comb[n][x][y] = # ways that we can start from (15,15), and after n steps,
+	// we end up in (x, y)
+	static int[][][] comb = new int[15][31][31];
+
 	public static void main(String[] args) {
 
-		// Gets file name from user input and creates a scanner of file
-		Scanner scanner = createScanner();
+		int i, row, col, j;
 
-	}
-
-	public static Scanner createScanner() {
-		// Takes input from user for file name
-		Scanner scanner = new Scanner(System.in);
-		String fileName = scanner.next();
-		scanner.close();
-
-		// Opens file from given file name
-		File file = new File(fileName);
-
-		try {
-			scanner = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		// initlialize all of comb to 0
+		for (i = 1; i < 15; i++) {
+			for (row = 1; row < 30; row++) {
+				for (col = 1; col < 30; col++) {
+					comb[i][row][col] = 0;
+				}
+			}
 		}
 
-		return scanner;
-	}
+		// Open the scanner to take in from System.in
+		Scanner scanner = new Scanner(System.in);
 
+		// While you continue to give it numbers
+		while (scanner.hasNextInt()) {
+
+			int toCalc = scanner.nextInt();
+
+			comb[0][15][15] = 1;
+
+			// Main logic
+			for (i = 1; i < toCalc + 1; i++) {
+				for (row = 1; row < 30; row++) {
+					for (col = 1; col < 30; col++) {
+
+						if (comb[i][row][col] != 0) {
+							// Stops it from calculating a value that has
+							// already been calculated and stored.
+						} else {
+
+							// For each i, row, and col.. it adds up the values
+							// of all of it's neighbors
+							for (j = 0; j < 6; j++) {
+								int cur_row = row + move[j][0];
+								int cur_col = col + move[j][1];
+								comb[i][row][col] += comb[i - 1][cur_row][cur_col];
+							}
+						}
+					}
+				}
+			}
+
+			System.out.println(comb[toCalc][15][15]);
+		}
+
+	}
 }
